6d84fdd7c180716c24c96a474829167e57683ec3
Anna Kochergina <akochergina92@gmail.com>
2023-08-25 22:18:50 +0300

    Initional commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        float calculation = service.calculate(55.0f, 1.63f);
+        System.out.println((int)calculation);
+    }
+}

0035350bf492281107dba8830396d0b7461f3418
Anna Kochergina <akochergina92@gmail.com>
2023-08-30 21:27:13 +0300

    refactoring

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public float calculate (float weight, float height) {   // два значения на вход вес и рост
+    public float calculate(float weight, float height) {   // два значения на вход вес и рост
         float bmi;
         bmi = weight / (height * height);
 
-        return (int)bmi;
+        return (int) bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         float calculation = service.calculate(55.0f, 1.63f);
-        System.out.println((int)calculation);
+        System.out.println((int) calculation);
     }
 }
